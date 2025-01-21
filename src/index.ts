25a900e57b2e78006fa460c4546552232403ceb9
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-01-22 03:44:54 +0530

    how to add datat

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
+import { PrismaClient } from "@prisma/client";
 
-const ithihas = "love of my life"
+const prisma = new PrismaClient()
+
+
+async function insertUser(username: string, password: string, firstName: string, lastName: string, email:string) {
+  
+   const response = await  prisma.user.create({
+        data:{
+            email,
+            username,
+            password,
+            firstName,
+            lastName
+            
+
+        }
+    })
+    console.log(response)
+}
+
+insertUser("sahithiyo","i love sahithi", "sahithi", "ata", "sahithiyo@gmail.com")
